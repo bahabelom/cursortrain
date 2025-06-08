ee4bfc92978e1153fd60f52524617d0647a6e8e4
bahabelom <bahabelomgebremedhn2@gmail.com>
2025-06-08 08:23:51 -0700

    Modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
-
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
